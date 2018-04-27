64b4f489a5560910d30ce6f091a20811f9c3ba6b
ArkMus <markus.andersson97@gmail.com>
2018-04-27 23:58:14 +0300

    Added feature, where user can mark pokemons as caught. Users can also see how many pokemons they have caught.

--- application/collections/models.py
+++ application/collections/models.py
@@ -8,23 +8,39 @@ class Collections(Base):
 
     name = db.Column(db.String(144), nullable=False)
     number = db.Column(db.String(144), nullable=False)
+    caught = db.Column(db.Boolean, nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
     def __init__(self, name, number, account_id):
         self.name = name
         self.number = number
+        self.caught = False
         self.account_id = account_id
 
     @staticmethod
     def find_users_collection(id, done=0):
-        stmt = text("SELECT collections.name, collections.number "
+        stmt = text("SELECT collections.number, collections.name, collections.caught, collections.id "
         "FROM collections LEFT JOIN account ON account.id = collections.account_id "
         "WHERE collections.account_id = :id;").params(done=done, id=id)
         res = db.engine.execute(stmt)
 
         response = []
         for row in res:
-            response.append({"number":row[0], "name":row[1]})
+            response.append({"number":row[0], "name":row[1], "caught":row[2], "id":row[3]})
 
         return response
+
+    @staticmethod
+    def how_many_caught(id, done=0):
+        stmt = text("SELECT count(collections.caught) "
+        "FROM collections LEFT JOIN account ON account.id = collections.account_id "
+        "WHERE collections.caught = 1 AND collections.account_id = :id;").params(done=done, id=id)
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"count":row[0]})
+
+        return response
+
--- application/collections/views.py
+++ application/collections/views.py
@@ -8,4 +8,12 @@ from application.collections.models import Collections
 @app.route("/collections/user/", methods=["GET"])
 @login_required
 def all_collections():
-    return render_template("collections/user.html", user_collection=Collections.find_users_collection(current_user.id))
+    return render_template("collections/user.html", user_collection=Collections.find_users_collection(current_user.id), user_count=Collections.how_many_caught(current_user.id))
+
+@app.route("/collections/user/<pokeid>/", methods=["POST"])
+def pokemon_set_caught(pokeid):
+    poke = Collections.query.filter_by(id=pokeid).first()
+    poke.caught = True
+    db.session().commit()
+
+    return redirect(url_for("all_collections"))
