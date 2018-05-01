3cc16a95ceb493e9709f2693775ac2b11f8da38c
ArkMus <markus.andersson97@gmail.com>
2018-05-01 16:13:04 +0300

    User can now release or remove pokemons from his/her collection.

--- application/collections/views.py
+++ application/collections/views.py
@@ -13,7 +13,25 @@ def all_collections():
 @app.route("/collections/user/<pokeid>/", methods=["POST"])
 def pokemon_set_caught(pokeid):
     poke = Collections.query.filter_by(id=pokeid).first()
-    poke.caught = 1
+    if poke.caught == 1:
+        poke.caught = 0
+    else:
+        poke.caught = 1
+    db.session().commit()
+
+    return redirect(url_for("all_collections"))
+
+@app.route("/collections/remove/", methods=["GET", "POST"])
+@app.route("/collections/remove/<pokeid>", methods=["GET", "POST"])
+@login_required
+def remove_pokemon(pokeid):
+    pokeToDelete = Collections.query.filter_by(id=pokeid).first()
+
+    if request.method == "GET":
+        return render_template("collections/remove.html", pokeid = pokeid, pokemon=pokeToDelete)
+    
+
+    db.session.delete(pokeToDelete)
     db.session().commit()
 
     return redirect(url_for("all_collections"))
